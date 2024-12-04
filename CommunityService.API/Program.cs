d9368648b7af54813bcea05a698544f05de44636
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2024-12-04 19:57:45 +0400

    structure and nugets

--- CommunityService.API/Program.cs
+++ CommunityService.API/Program.cs
@@ -1,10 +1,16 @@
 using CommunityService.Persistence.Contexts;
+using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.AddConsole();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFastEndpoints();
 
 builder.Services.AddDbContext<CommunityContext>(opts =>
     opts.UseInMemoryDatabase("CommunityInMemo"));
@@ -13,9 +19,11 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(opts => opts.RouteTemplate = "openapi/{documentName}.json");
+    app.MapScalarApiReference();
 }
+
+app.UseFastEndpoints();
 
 app.UseHttpsRedirection();
 
