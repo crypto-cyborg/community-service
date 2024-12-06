c3b0f0f3d7d6c63a8ec3bea125825289ee7c66b3
tng <m.farm4nov@gmail.com>
2024-12-06 16:18:28 +0400

    feat: entities extensions

---
+++ CommunityService.Core/Extensions/PostExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CommunityService.Core.Models;
+
+namespace CommunityService.Core.Extensions;
+
+public static class PostExtensions
+{
+    public record PostReadDto(
+        Guid Id,
+        UserExtensions.UserReadDto User,
+        IEnumerable<Tag> Tags,
+        DateTimeOffset Time,
+        string Topic,
+        string Text,
+        IEnumerable<ReactionExtensions.ReactionReadDto> Reactions);
+
+    public static PostReadDto MapToResponse(this Post post)
+        => new(post.Id, post.User.MapToResponse(), post.Tags, post.Time, post.Topic, post.Text,
+            post.Reactions.MapToResponse());
+}
---
+++ CommunityService.Core/Extensions/UserExtensions.cs
@@ -0,0 +1,11 @@
+﻿using CommunityService.Core.Models;
+
+namespace CommunityService.Core.Extensions;
+
+public static class UserExtensions
+{
+    public record UserReadDto(Guid UserId, string Username, string ImageUrl);
+
+    public static UserReadDto MapToResponse(this User user) =>
+        new(user.UserId, user.Username, user.ImageUrl);
+}
